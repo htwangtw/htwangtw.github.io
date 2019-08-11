0b4a81b1abdbda24302555d690d78b09266a4ed0
Hao-Ting <haoting.wang@york.ac.uk>
2019-08-11 06:38:50 +0100

    .gitignore is now working

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"some content here  ","categories": ["blog","travel"],
-        "tags": ["hot","summer"],
-        "url": "http://localhost:4000/blog/travel/2018/11/01/place-holder.html",
-        "teaser":null}]
