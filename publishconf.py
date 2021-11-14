96ea4a7010840bf3942d7494919001ef3ee74029
Hao-Ting Wang <htwangtw@gmail.com>
2021-11-14 09:09:13 -0500

    url

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://htwangtw.github.io'
+SITEURL = 'https://wanghaoting.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
